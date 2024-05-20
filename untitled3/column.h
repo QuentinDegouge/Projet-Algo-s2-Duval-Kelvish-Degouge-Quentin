07a82a06c9077c7a3aa7066365b0fdf84577b653
Kelvish <150790778+K3lvish@users.noreply.github.com>
2024-05-20 16:39:25 +0200

    Update column.h

--- untitled3/column.h
+++ untitled3/column.h
@@ -2,41 +2,43 @@
 #ifndef UNTITLED3_COLUMN_H
 #define UNTITLED3_COLUMN_H
 
-/**
-* Create a column
-* @param1 : Column title
-* @return : Pointer to created column
-*/
-COLUMN *create_column(char* title);
+typedef struct{
+    char titre[20];
+    int tp;
+    int tl;
+    int* tableau;
+} COLUMN;
+
+COLUMN *create_column(char *title);
 
 
-/**
-* @brief : Add a new value to a column
-* @param1 : Pointer to a column
-* @param2 : The value to be added
-* @return : 1 if the value is added 0 otherwise
-*/
-int insert_value(COLUMN* col, int value);
+int insert_value(COLUMN *col, int value);
 
 
-
-/**
-* @brief : Free allocated memory
-* @param1 : Pointer to a column
-*/
 void delete_column(COLUMN **col);
 
 
-
-/**
-* @brief: Print a column content
-* @param: Pointer to a column
-*/
 void print_col(COLUMN* col);
 
 
+int nb_occurences(COLUMN *col, int x);
 
 
+int value_pos(COLUMN *col, int x);
 
+
+int val_sup(COLUMN *col, int x);
+
+
+int val_inf(COLUMN *col, int x);
+
+
+int val_egal(COLUMN *col, int x);
+
+
+void print_liml(COLUMN *, int x);
+
+
+void add_line(COLUMN *col, int x);
 
 #endif //UNTITLED3_COLUMN_H
